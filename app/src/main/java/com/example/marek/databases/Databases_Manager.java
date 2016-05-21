9df536c9ce229966c2a433f7dbacfacc18046e59
marpol1992 <marpol199217@gmail.com>
2016-05-22 00:33:53 +0200

    Dodanie metody usuwającej podane id, dodanie metody usuwającej całą baze a nastęnie kasowanie licznika zapisanego w tablicy sqlite_sequence

--- app/src/main/java/com/example/marek/databases/Databases_Manager.java
+++ app/src/main/java/com/example/marek/databases/Databases_Manager.java
@@ -5,19 +5,23 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by marek on 2016-05-21.
  */
 public class Databases_Manager extends SQLiteOpenHelper {
-    public Databases_Manager(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+    public String table_name;
+    String[] kolumns= {"nr","nazwa", "adress","tak", "nie"};
+    public Databases_Manager(Context context, String name, SQLiteDatabase.CursorFactory factory, int version, String table_name) {
         super(context, name, factory, version);
+        this.table_name = table_name;
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(
-                "create table restaurant(" +
+                "create table" +table_name+"(" +
                         "nr integer primary key autoincrement," +
                         "nazwa text," +
                         "adress text," +
@@ -39,14 +43,42 @@ public class Databases_Manager extends SQLiteOpenHelper {
         values.put("adress", adress);
         values.put("tak",tak);
         values.put("nie", nie);
-        databases.insertOrThrow("restaurant", null, values);
+        databases.insertOrThrow(table_name, null, values);
 
     }
 
     public Cursor read_all_kolumns(){
         String[] kolumns= {"nr","nazwa", "adress","tak", "nie"};
         SQLiteDatabase database = getReadableDatabase();
-        Cursor cursor = database.query("restaurant", kolumns,null,null,null,null,null);
+        Cursor cursor = database.query(table_name, kolumns,null,null,null,null,null);
         return cursor;
+    }
+
+    public void delete_row(int id){
+        SQLiteDatabase database = getWritableDatabase();
+        String[] arguments = {"" + id};
+        database.delete(table_name,"nr=?",arguments);
+
+    }
+
+    public void delete_allRow(){
+        SQLiteDatabase databases = getReadableDatabase();
+        String[] kolumns = {"MAX(nr) as nr"};
+        Cursor cursor = databases.query(table_name, kolumns, null, null, null, null, null);
+        int size = 0;
+        while(cursor.moveToNext()) {
+            size = cursor.getInt(0);
+            Log.d("dane bazy",Integer.toString(size));
+        }
+
+
+      SQLiteDatabase database = getWritableDatabase();
+        for(long i= 0;i<size+1;i++) {
+            String[] arguments = {"" + i};
+            database.delete(table_name, "nr=?",arguments);
+        }
+        
+        database.delete("sqlite_sequence",null,null);
+      cursor.close();
     }
 }
--- app/src/main/java/com/example/marek/databases/MainActivity.java
+++ app/src/main/java/com/example/marek/databases/MainActivity.java
@@ -3,19 +3,23 @@ package com.example.marek.databases;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 public static final String DBNAME = "test.db";
+public static final String TABLE_NAME = "restaurant";
+    Databases_Manager databases_manager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView textView = (TextView) findViewById(R.id.textView);
-        Databases_Manager databases_manager = new Databases_Manager(this,DBNAME,null,1);
+        databases_manager = new Databases_Manager(this,DBNAME,null,2, TABLE_NAME);
         databases_manager.add_restaurant("Kolos Kebab","Zwierzyniecka 8",123,12);
-        databases_manager.add_restaurant("KFC","Mickiewicza 8",133,342);
+        databases_manager.add_restaurant("KFC", "Mickiewicza 8", 133, 342);
         databases_manager.add_restaurant("Burger King","Leśna 9",123,132);
+        //textView.setText(" ");
         Cursor cursor = databases_manager.read_all_kolumns();
         while(cursor.moveToNext()){
             int nr = cursor.getInt(0);
@@ -25,6 +29,11 @@ public static final String DBNAME = "test.db";
             int nie = cursor.getInt(4);
             textView.setText(textView.getText()+ "\n" + nr + " " + name + " " + adress + " " + tak + " " + nie);
         }
+
+    }
+
+    public void cllear_all(View view) {
+        databases_manager.delete_allRow();
 
     }
 }
